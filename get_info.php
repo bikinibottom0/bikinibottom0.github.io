3e1e75ecef4effe49a5eb9d9c191dc83e0f965f3
bikinibottom0 <84742354+bikinibottom0@users.noreply.github.com>
2021-05-25 07:29:05 +0300

    Add files via upload

---
+++ get_info.php
@@ -0,0 +1,9 @@
+<?php
+$list=array($_POST['user_name'],$_POST['user_mail'],$_POST['user_phone'],$_POST['user_message']);
+$file=fopen('clients.csv','a+');
+fputcsv($file, $list);
+fclose($file);
+echo 'Успешно\n';
+echo '<a href="index.html">Link Here</a>';
+?>
+
